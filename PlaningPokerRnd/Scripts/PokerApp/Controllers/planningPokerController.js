005f34803665f43ccbaec9632f0cc56b5e14a2ff
Mikalojus <m.galminas@ba.lt>
2016-11-18 16:43:31 +0200

    angular fix

--- PlaningPokerRnd/Scripts/PokerApp/Controllers/planningPokerController.js
+++ PlaningPokerRnd/Scripts/PokerApp/Controllers/planningPokerController.js
@@ -5,7 +5,23 @@
         "$scope", "apiService",
         function ($scope, apiService) {
 
+            function loadData() {
+                apiService.get(application.config().getApiUrl("Poker/Rooms/"), null,
+                    loadCompleted,
+                    loadFailed);
+            }
 
+            function loadCompleted(result) {
+                $scope.data = result.data;
+                $scope.origmodel = angular.copy(result.data);
+                $scope.loading = false;
+            }
+
+            function loadFailed() {
+                return;
+            }
+
+            loadData();
         }
     ]);
 })(angular.module("pokerApp"));
