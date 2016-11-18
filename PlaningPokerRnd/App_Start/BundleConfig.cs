cbbd4ad9e93dddcbe653f9ecfb5f66cf12331f81
Mikalojus <m.galminas@ba.lt>
2016-11-18 15:52:39 +0200

    Bundles

--- PlaningPokerRnd/App_Start/BundleConfig.cs
+++ PlaningPokerRnd/App_Start/BundleConfig.cs
@@ -6,11 +6,20 @@ namespace PlaningPokerRnd
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                    "~/Scripts/angular.min.js",
+                    "~/Scripts/angular-route.min.js",
+                    "~/Scripts/angular-validator.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/Materialize/materialize.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                  "~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Materialize").Include(
+                 "~/Content/Materialize/materialize.css"));
         }
     }
 }
