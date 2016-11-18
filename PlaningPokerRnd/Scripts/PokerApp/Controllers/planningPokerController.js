f3742ce5aa590618419d11fd3212a1ae4bc3f164
Darius Butkevicius <d.butkevicius@ba.lt>
2016-11-18 16:57:46 +0200

    asf

--- PlaningPokerRnd/Scripts/PokerApp/Controllers/planningPokerController.js
+++ PlaningPokerRnd/Scripts/PokerApp/Controllers/planningPokerController.js
@@ -2,14 +2,18 @@
     "use strict";
 
     app.controller("planningPokerController", [
-        "$scope", "apiService",
+        "$scope", "apiService", "$http",
         function ($scope, apiService, $http) {
 
             
+
             function loadData() {
-                apiService.get(application.config().getApiUrl("Poker/Rooms/"), null,
-                    loadCompleted,
-                    loadFailed);
+
+                $http.get("api/Poker/Rooms/")
+                    .then(function (result) {
+                        $scope.rooms = result.data;
+                        console.log(resul.data);
+                    });
             }
 
             function loadCompleted(result) {
@@ -23,66 +27,9 @@
             }
 
             loadData();
-                'use strict';
-
-                app.factory('apiService', [
-                    '$http', '$location', '$rootScope',
-                    function ($http, $location, $rootScope) {
-                        var service = {
-                            get: get,
-                            post: post,
-                            remove: remove
-                        };
-
-                        function get(url, config, success, failure) {
-                            return $http.get(url, config)
-                                .then(function (result) {
-                                    success(result);
-                                }, function (error) {
-                                    if (error.status == '401') {
-                                        $rootScope.previousState = $location.path();
-                                        $location.path('/login');
-                                    } else if (failure != null) {
-                                        failure(error);
-                                    }
-                                });
-                        }
-
-                        function post(url, data, success, failure) {
-                            return $http.post(url, data)
-                                .then(function (result) {
-                                    success(result);
-                                }, function (error) {
-                                    if (error.status == '401') {
-                                        $rootScope.previousState = $location.path();
-                                        $location.path('/login');
-                                    } else if (failure != null) {
-                                        failure(error);
-                                    }
-                                });
-                        }
-
-                        function remove(url, success, failure) {
-                            return $http.delete(url)
-                                .then(function (result) {
-                                    success(result);
-                                }, function (error) {
-                                    if (error.status == '401') {
-                                        $rootScope.previousState = $location.path();
-                                        $location.path('/login');
-                                    } else if (failure != null) {
-                                        failure(error);
-                                    }
-                                });
-                        }
-
-                        return service;
-                    }
-                ]);
-
-            })(angular.module('common.core'));
+              
             $scope.joinRoom = function() {
-                $http.get()
+                $http.get();
             };
         }
     ]);
