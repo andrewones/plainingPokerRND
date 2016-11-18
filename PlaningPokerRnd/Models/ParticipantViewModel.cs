c448fbb9acabe33cfcf7b3c8bc801b5011ca14e5
Andrej Verchovych <a.verchovych@baltic-amadeus.lt>
2016-11-18 15:44:02 +0200

    web api skeleton

---
+++ PlaningPokerRnd/Models/ParticipantViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PlaningPokerRnd.Models
+{
+    public class ParticipantViewModel
+    {
+        public string Nick { get; set; }
+        public string Vote { get; set; }
+    }
+}
---
+++ PlaningPokerRnd/Models/RoomListItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PlaningPokerRnd.Models
+{
+    public class RoomListItemViewModel
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
